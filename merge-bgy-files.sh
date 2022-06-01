b0bdc5de55286d92067548d1863d49922b10ad66
tropicalmentat <gabtorres1011@gmail.com>
2022-06-01 18:41:51 +0800

    Finalized merge script

--- merge-bgy-files.sh
+++ merge-bgy-files.sh
@@ -1,5 +1,9 @@
-for file in data/raw/official_philippines_shapefile_data_2016/*.*;
+for file in data/raw/official_philippines_shapefile_data_2016/*.shp;
 
-do echo $file;
+do 
+
+echo $file
+
+ogr2ogr -f "ESRI Shapefile" -update -append merge.shp "${file}"
 
 done
